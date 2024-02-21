85f29d1b343573f2323076db43bd85d3bf8745cb
Shlomo Friedman <sz.friedman123@gmail.com>
2024-02-21 13:11:34 -0500

    added progress bar and fixed buttons

--- pdf_to_img_gui.py
+++ pdf_to_img_gui.py
@@ -5,9 +5,10 @@ import PySimpleGUI as sg
 # All the stuff inside your window.
 layout = [  [sg.Text('Input file'), sg.InputText(key='-INPUT-FOLDER-'), 
              #sg.Button('select_file', image_filename=r"C:\Users\shlomof\Documents\code\pdf2image_gui\icons\add-file-8.png", image_size=(24, 24), key='-SELECT_FILE-'), 
-             sg.Listbox(values=['PNG', 'JPG'], default_values='PNG' , size=(6, 2), key='-FILETYPE-', no_scrollbar=True)],
+             sg.Listbox(values=['PNG', 'JPG'], default_values='PNG' , size=(6, 2), key='-FILETYPE-', tooltip='Output file format', no_scrollbar=True),
+             sg.Listbox(values=['Low', 'High'], default_values='Low' , size=(6, 2), key='-QUALITY-', tooltip='Quality setting', no_scrollbar=True)],
             [sg.Text('Output folder path'), sg.InputText(key='-OUTPUT-FOLDER-')],
-            [sg.ProgressBar(max_value=(10), key='-PROGRESS_BAR-')],
+            [sg.ProgressBar(max_value=(10), size=(48, 10), key='-PROGRESS_BAR-')],
             [sg.Button('Convert'), sg.Button('Quit')] ]
 count = 0
 # Create the Window
@@ -18,7 +19,8 @@ window = sg.Window(title='PDF to IMG',
 
 
 #run script
-def convert_on_press (window, event, values):
+def convert_on_press (window, event, values, user_dpi='', quality='',
+                      file_extension='', file_type='', progress=''):
     
     print(values['-INPUT-FOLDER-'])
     if str(values['-INPUT-FOLDER-']) == '':
@@ -51,29 +53,44 @@ def convert_on_press (window, event, values):
 
             try:
                 for x in range(len(filelist)):
+                    
+                    y = len(filelist)
+
                     print("did you get here1")
                     filename = filelist[x].rsplit('/')
                     filename = filename[-1].rsplit('.')
                     filename = filename[0]#.replace(".", "_").strip(']').strip("'")
-                    images = convert_from_path(filelist[x], dpi=800, poppler_path=r"C:\Program Files\poppler-23.11.0\Library\bin")
+                    if values['-QUALITY-'] == ['Low']:
+                        user_dpi = 200
+                        quality = ''
+                    if values['-QUALITY-'] == ['High']:
+                        user_dpi = 600
+                        quality = 'hq_'
+                    images = convert_from_path(filelist[x], dpi=user_dpi, poppler_path=r"C:\Program Files\poppler-23.11.0\Library\bin")
+                    y = 10 / (len(images))
                     for i in range(len(images)):
-                        
+                        y += y
+                        print(y)
+                        window['-PROGRESS_BAR-'].update(y)
+
                         # Save pages as images in the pdf
                         print("did you get here2")
                         print(values['-FILETYPE-'])
+                        
                         if values['-FILETYPE-'] == ['JPG']:
-                            print(values['-FILETYPE-'])
-                            images[i].save(user_output_folder + '\\' + str(filename) + '_' + str(i) +'.jpg', 'JPEG')
-                            pass
+                            file_extension = '.jpg'
+                            file_type = 'JPEG'
 
-                        elif values['-FILETYPE-'] == ['PNG']:
-                            print(values['-FILETYPE-'])
-                            images[i].save(user_output_folder + '\\' + str(filename) + '_' + str(i) +'.png', 'PNG')
-                            pass
-
+                        if values['-FILETYPE-'] == ['PNG']:
+                            file_extension = '.png'
+                            file_type = 'PNG'
                         else:
-                            images[i].save(user_output_folder + '\\' + str(filename) + '_' + str(i) +'.png', 'PNG')
-                            pass
+                            quality = ''
+                            file_extension = '.jpeg'
+                            file_type = 'JPEG'
+
+                        images[i].save(user_output_folder + '\\' + str(filename) + '_' + quality + str(i) + file_extension, file_type)
+                        pass
 
                     sg.popup(f"{filename} is done!")
 
@@ -88,7 +105,7 @@ def convert_on_press (window, event, values):
             pass
 
 
-
+progress = 0
            
 
 while True:
@@ -97,5 +114,6 @@ while True:
         break
     if event == 'Convert':
         convert_on_press(window, event, values)
+        window['-PROGRESS_BAR-'].update(0)
     #if event == 'select_file':
       #  sg.popup_get_file()
